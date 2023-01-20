a48b5ced7cd78c40f56f1bb0b61d0c67f3bd5a4a
semir <seper56@gmail.com>
2023-01-19 21:13:37 -0500

    .

--- assets/javascript.js
+++ assets/javascript.js
@@ -10,6 +10,7 @@
 //     });
 // });
 const button = document.getElementById('button')
+const OFFSET = 100
 
 button.addEventListener('click', () => {
     alert('woooooow really!')
@@ -21,10 +22,35 @@ document.addEventListener('mousemove' ,(e) => {
     const x = e.pageX
     const y = e.pageY
     console.log(x, y)
-    const buttonbox = button.getBoundingClientRect()
-    const hotizontalDistanacfrom = distancefromceneter(button.x, x 
-    buttonbox.width) 
+    const buttonBox = button.getBoundingClientRect()
+    const hotizontalDistanacfrom = distancefromceneter(button.x, x, 
+    buttonBox.width) 
+    const verticalDistanacfrom = distancefromceneter(button.x, x, 
+        buttonBox.height)
+    const hotizontaloffset = buttonBox.width / 2 + OFFSET
+    const verticaloffset = buttonBox.height / 2 + OFFSET
+    if (Math.abs(hotizontalDistanacfrom) <= hotizontaloffset && Math.abs (verticalDistanacfrom) <= (verticaloffset) {
+        setButtonPosition(
+            buttonBox.x + hotizontaloffset / hotizontalDistanacfrom * 10
+            ,buttonBox.y + verticaloffset / verticalDistanacfrom * 10
+        )
+        
+    } 
+    
+    
+
 })
+function setButtonPosition(top, left) {
+    const windowBox = document.body.getBoundingClientRect()
+    const buttonBox = button.getBoundingClientRect()
+
+    if(disstanceFromCenter(left, windowBox.left, buttonBox.height) < 0) {
+        left = windowBox.right - button.width - OFFSET
+    }
+
+    button.style.top  = '${top}px'
+    button.style.left = '${left}px'
+}
 
 function disstanceFromCenter(boxPosition, mousePosition, boxSize) {
     return boxPosition - mousePosition + boxSize / 2
