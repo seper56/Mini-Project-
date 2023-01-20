4b3483337ba403d155a401f2444b7339e9724776
semir <seper56@gmail.com>
2023-01-19 20:42:56 -0500

    .

--- assets/javascript.js
+++ assets/javascript.js
@@ -9,3 +9,23 @@
 //         }
 //     });
 // });
+const button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    alert('woooooow really!')
+    window.close()
+
+})
+
+document.addEventListener('mousemove' ,(e) => {
+    const x = e.pageX
+    const y = e.pageY
+    console.log(x, y)
+    const buttonbox = button.getBoundingClientRect()
+    const hotizontalDistanacfrom = distancefromceneter(button.x, x 
+    buttonbox.width) 
+})
+
+function disstanceFromCenter(boxPosition, mousePosition, boxSize) {
+    return boxPosition - mousePosition + boxSize / 2
+}
