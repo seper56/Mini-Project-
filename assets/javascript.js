acdd92e760467e2851476b8ab8569f992c544efa
semir <seper56@gmail.com>
2023-01-19 20:26:22 -0500

    .

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,17 +1,11 @@
 
-var myfunction = function(){
-    var myVar = "button";
-};
-
-console.log(myVar);
-
-$(function(){
-    $("button").on({
-        mouseover:function(){
-            $(this).css({
-                left:(Math.random()*200)+"px",
-                top:(Math.random()*200)+"px",
-            });
-        }
-    });
-});
+// $(function(){
+//     $("button").on({
+//         mouseover:function(){
+//             $(this).css({
+//                 left:(Math.random()*200)+"px",
+//                 top:(Math.random()*200)+"px",
+//             });
+//         }
+//     });
+// });
