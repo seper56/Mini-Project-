ea96cff7d66ced82414447332f5041eb8a49a3ce
semir <seper56@gmail.com>
2023-01-19 21:41:07 -0500

    .

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,55 +1,76 @@
+// JavaScript to handle button movement
+const noButton = document.getElementById("no-button");
+let mouseX;
+let mouseY;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+setInterval(() => {
+  const buttonRect = noButton.getBoundingClientRect();
+  const buttonX = buttonRect.left + buttonRect.width / 2;
+  const buttonY = buttonRect.top + buttonRect.height / 2;
+
+  const distanceX = mouseX - buttonX;
+  const distanceY = mouseY - buttonY;
+
+  noButton.style.left = distanceX / 20 + "px";
+  noButton.style.top = distanceY / 20 + "px";
+}, 20);
 
 
-const button = document.getElementById('button')
-const OFFSET = 100
+// const button = document.getElementById('button')
+// const OFFSET = 100
 
-button.addEventListener('click', () => {
-    alert('woooooow really!')
-    window.close()
+// button.addEventListener('click', () => {
+//     alert('woooooow really!')
+//     window.close()
 
-})
+// })
 
-document.addEventListener('mousemove' ,(e) => {
-    const x = e.pageX
-    const y = e.pageY
-    console.log(x, y)
-    const buttonBox = button.getBoundingClientRect()
-    const hotizontalDistanacfrom = disstanceFromCenter(button.x, x, buttonBox.width) 
-    const verticalDistanacfrom = disstanceFromCenter(button.x, x, buttonBox.height)
-    const hotizontaloffset = buttonBox.width / 2 + OFFSET
-    const verticaloffset = buttonBox.height / 2 + OFFSET
-    if (Math.abs(hotizontalDistanacfrom) <= hotizontaloffset && Math.abs (verticalDistanacfrom) <= (verticaloffset)) {
-        setButtonPosition(
-            buttonBox.x + hotizontaloffset / hotizontalDistanacfrom * 10
-            ,buttonBox.y + verticaloffset / verticalDistanacfrom * 10
-        )
+// document.addEventListener('mousemove' ,(e) => {
+//     const x = e.pageX
+//     const y = e.pageY
+//     console.log(x, y)
+//     const buttonBox = button.getBoundingClientRect()
+//     const hotizontalDistanacfrom = disstanceFromCenter(button.x, x, buttonBox.width) 
+//     const verticalDistanacfrom = disstanceFromCenter(button.x, x, buttonBox.height)
+//     const hotizontaloffset = buttonBox.width / 2 + OFFSET
+//     const verticaloffset = buttonBox.height / 2 + OFFSET
+//     if (Math.abs(hotizontalDistanacfrom) <= hotizontaloffset && Math.abs (verticalDistanacfrom) <= (verticaloffset)) {
+//         setButtonPosition(
+//             buttonBox.x + hotizontaloffset / hotizontalDistanacfrom * 10
+//             ,buttonBox.y + verticaloffset / verticalDistanacfrom * 10
+//         )
         
-    } 
+//     } 
     
     
 
-})
-function setButtonPosition(top, left) {
-    const windowBox = document.body.getBoundingClientRect()
-    const buttonBox = button.getBoundingClientRect()
+// })
+// function setButtonPosition(top, left) {
+//     const windowBox = document.body.getBoundingClientRect()
+//     const buttonBox = button.getBoundingClientRect()
 
-    if(disstanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
-        left = windowBox.right - button.width - OFFSET
-    }
-    if(disstanceFromCenter(left, windowBox.right, buttonBox.width) < 0) {
-        left = windowBox.left + OFFSET
-    }
-    if(disstanceFromCenter(top, windowBox.right, buttonBox.height) < 0) {
-        top = windowBox.bottom - buttonBox.height - OFFSET
-    }
-    if(disstanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
-        top = windowBox.top + OFFSET
-    }
+//     if(disstanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+//         left = windowBox.right - button.width - OFFSET
+//     }
+//     if(disstanceFromCenter(left, windowBox.right, buttonBox.width) < 0) {
+//         left = windowBox.left + OFFSET
+//     }
+//     if(disstanceFromCenter(top, windowBox.right, buttonBox.height) < 0) {
+//         top = windowBox.bottom - buttonBox.height - OFFSET
+//     }
+//     if(disstanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+//         top = windowBox.top + OFFSET
+//     }
 
-    button.style.top  = '${top}px'
-    button.style.left = '${left}px'
-}
+//     button.style.top  = "${top}px"
+//     button.style.left = "${left}px"
+// }
 
-function disstanceFromCenter(boxPosition, mousePosition, boxSize) {
-    return boxPosition - mousePosition + boxSize / 2
-}
+// function disstanceFromCenter(boxPosition, mousePosition, boxSize) {
+//     return boxPosition - mousePosition + boxSize / 2
+// }
